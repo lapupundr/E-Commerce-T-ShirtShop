9ca493275a9e76434dc4cfa68da6263926c88f1d
Tatsiana Zatonets <koshka.tan@gmail.com>
2022-10-20 00:39:37 +0300

    change href in template departmentList

--- index.php
+++ index.php
@@ -3,12 +3,8 @@
 declare(strict_types=1);
 
 use Ecommerce\Core\Controller\FrontController;
-use Ecommerce\Core\Routing\Router;
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-//$router = new Router();
-//$router->match();
 
 $router = new FrontController();
 $router->execute();
